22e31d0e0fe14c4b89dce72aa228c62ff97b416a
issayoume2012-ui <issayoume2012@gmail.com>
2026-05-12 23:31:35 +0000

    Update youmissa.py

--- youmissa.py
+++ youmissa.py
@@ -11,32 +11,42 @@ with st.sidebar:
         
     st.subheader("Disponible pour un entretien. Vous pouvez me joindre facilement par Mail")
 st.header("▪️Formations")
-st.write("👉🏿2024-2027 : Licence en Agronomie, Alimentaire et Nutritionelle, Université Amadou Makhtar Mbow de Dakar")
-st.write("👉🏿2023-2024 : Bachelier Scientifique en Serie S2, Lycée d'Excellence Privé Aimé Césaire, Saint-Louis")
-
+st.write("- 2024-2027 : Licence en Agronomie, Alimentaire et Nutritionelle, Université Amadou Makhtar Mbow de Dakar")
+st.write("- 2023-2024 : Bachelier Scientifique en Serie S2, Lycée d'Excellence Privé Aimé Césaire, Saint-Louis")
+st.markdown ("""
+    ___________________________________________________________________________
+    """)
 
 st.header("▪️Compétences Techniques")
-st.subheader("👉🏿 Analyse et suivi des récoltes")
+st.subheader("- Analyse et suivi des récoltes")
 st.write("Capable d’analyser et de suivre les récoltes grâce aux biomathématiques, aux nouvelles technologies, à la télédétection et à la cartographie pour améliorer les rendements agricoles.")
-st.subheader("👉🏿 Étude et gestion des sols agricoles")
+st.subheader("- Étude et gestion des sols agricoles")
 st.write("Compétent dans l’analyse, le diagnostic et la remédiation des sols à travers la pédologie agricole afin d’améliorer leur fertilité et leur productivité.")
-st.subheader("👉🏿 Physiologie animale et végétale")
+st.subheader("- Physiologie animale et végétale")
 st.write("Bonne connaissance du fonctionnement des plantes et des animaux à travers la physiologie animale et végétale, la botanique, la zoologie, la nutrition et la reproduction.")
-st.subheader("👉🏿 Amélioration des cultures par les technologies agricoles")
+st.subheader("- Amélioration des cultures par les technologies agricoles")
 st.write("Capable d’améliorer les cultures grâce aux biotechnologies, à la bio-informatique, à l’entomologie, à l’irrigation et à la fertilisation.")
-st.subheader("👉🏿 Sociologie rurale et gestion d’entreprise")
+st.subheader("- Sociologie rurale et gestion d’entreprise")
 st.write("Compétent en sociologie rurale ainsi qu’en gestion administrative et économique des entreprises agricoles.")
+st.markdown ("""
+    ___________________________________________________________________________
+    """)
 st.header("▪️Compétences Communicationnelles")
-st.write("👉🏿Capable de dialoguer et de trouver des solutions consensuelles avec les acteurs agricoles")
-st.write("👉🏿Capable d'écrire un rapport, de préparer et de présenter des documents sous format PowerPoint en français et en angalais")
- 
+st.write("- Capable de dialoguer et de trouver des solutions consensuelles avec les acteurs agricoles")
+st.write("- Capable d'écrire un rapport, de préparer et de présenter des documents sous format PowerPoint en français et en angalais")
+st.markdown ("""
+    ___________________________________________________________________________
+    """)
 st.header("▪️Compétences Personnelles")
-st.write("👉🏿Capacité à travailler en Groupe et à s'adapter vite")
-st.write("👉🏿Capacité à communiquer avec les différents acteurs")
-st.write("👉🏿Capable de travailler sous pression")
-st.write("👉🏿Leadership")
-st.write("👉🏿Ponctualité")
-st.write("👉🏿Autonomie")
+st.write("- Capacité à travailler en Groupe et à s'adapter vite")
+st.write("- Capacité à communiquer avec les différents acteurs")
+st.write("- Capable de travailler sous pression")
+st.write("- Leadership")
+st.write("- Ponctualité")
+st.write("- Autonomie")
+st.markdown ("""
+    ___________________________________________________________________________
+    """)
 st.header("▪️Compétence Numérique")
-st.write("👉🏿Capable d'utiliser et d'exploiter Word, Excel,PowerPoint, Canva avec un Niveau Approfondie")
-st.write("👉🏿Capable d'utiliser et d'exploiter Python, AlgoBox, Jupiterlab, JupiterNoteBook avec niveau débutant")
+st.write("- Capable d'utiliser et d'exploiter Word, Excel,PowerPoint, Canva avec un Niveau Approfondie")
+st.write("- Capable d'utiliser et d'exploiter Python, AlgoBox, Jupiterlab, JupiterNoteBook, googlo colab avec niveau débutant")
